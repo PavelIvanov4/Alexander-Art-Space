245310b257fa8d00c428aab4ab2d4339a0df9372
PavelIvanov4 <94687008+PavelIvanov4@users.noreply.github.com>
2023-07-07 16:15:37 +0300

    upd: widened the script

--- script.js
+++ script.js
@@ -20,3 +20,90 @@ menu[2].addEventListener('click', () => {
 	part2.style.display = 'none';
 	part3.style.display = 'block';
 })
+
+let view = document.getElementsByClassName('view');
+let back = document.getElementsByClassName('back');
+
+view[0].onclick = function () {
+	document.getElementById('popup1').style.display = 'block';
+	document.getElementsByTagName('main')[0].style.display = 'none';
+	document.getElementsByTagName('header')[0].style.display = 'none';
+	window.scrollTo(0, 0);
+}
+
+back[0].onclick = function () {
+	document.getElementById('popup1').style.display = 'none';
+	document.getElementsByTagName('main')[0].style.display = 'block';
+	document.getElementsByTagName('header')[0].style.display = 'block';
+	window.scrollTo(0, 100);
+}
+
+view[1].onclick = function () {
+	document.getElementById('popup2').style.display = 'block';
+	document.getElementsByTagName('main')[0].style.display = 'none';
+	document.getElementsByTagName('header')[0].style.display = 'none';
+	window.scrollTo(0, 0);
+}
+
+back[1].onclick = function () {
+	document.getElementById('popup2').style.display = 'none';
+	document.getElementsByTagName('main')[0].style.display = 'block';
+	document.getElementsByTagName('header')[0].style.display = 'block';
+	window.scrollTo(0, 100);
+}
+
+view[2].onclick = function () {
+	document.getElementById('popup3').style.display = 'block';
+	document.getElementsByTagName('main')[0].style.display = 'none';
+	document.getElementsByTagName('header')[0].style.display = 'none';
+	window.scrollTo(0, 0);
+}
+
+back[2].onclick = function () {
+	document.getElementById('popup3').style.display = 'none';
+	document.getElementsByTagName('main')[0].style.display = 'block';
+	document.getElementsByTagName('header')[0].style.display = 'block';
+	window.scrollTo(0, 100);
+}
+
+view[3].onclick = function () {
+	document.getElementById('popup4').style.display = 'block';
+	document.getElementsByTagName('main')[0].style.display = 'none';
+	document.getElementsByTagName('header')[0].style.display = 'none';
+	window.scrollTo(0, 0);
+}
+
+back[3].onclick = function () {
+	document.getElementById('popup4').style.display = 'none';
+	document.getElementsByTagName('main')[0].style.display = 'block';
+	document.getElementsByTagName('header')[0].style.display = 'block';
+	window.scrollTo(0, 100);
+}
+
+view[4].onclick = function () {
+	document.getElementById('popup5').style.display = 'block';
+	document.getElementsByTagName('main')[0].style.display = 'none';
+	document.getElementsByTagName('header')[0].style.display = 'none';
+	window.scrollTo(0, 0);
+}
+
+back[4].onclick = function () {
+	document.getElementById('popup5').style.display = 'none';
+	document.getElementsByTagName('main')[0].style.display = 'block';
+	document.getElementsByTagName('header')[0].style.display = 'block';
+	window.scrollTo(0, 100);
+}
+
+view[5].onclick = function () {
+	document.getElementById('popup6').style.display = 'block';
+	document.getElementsByTagName('main')[0].style.display = 'none';
+	document.getElementsByTagName('header')[0].style.display = 'none';
+	window.scrollTo(0, 0);
+}
+
+back[5].onclick = function () {
+	document.getElementById('popup6').style.display = 'none';
+	document.getElementsByTagName('main')[0].style.display = 'block';
+	document.getElementsByTagName('header')[0].style.display = 'block';
+	window.scrollTo(0, 100);
+}
